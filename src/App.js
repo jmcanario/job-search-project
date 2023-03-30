a5fc802f2b960d8392615d8c8e57aa7de6894fa4
Juan Manuel Armas <36412631+jmcanario@users.noreply.github.com>
2023-03-30 20:04:44 +0100

    correct

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-import { createTheme, CssBaseline, Paper, ThemeProvider } from '@mui/material';
-import Menu from './components/Menu/Menu';
-import Dashboard from './components/Dashboard';
-
+import { createTheme, CssBaseline, Paper, ThemeProvider } from "@mui/material";
+import Menu from "./components/Menu/Menu";
+import Dashboard from "./components/Dashboard";
 
 const themeColor = createTheme({
   palette: {
     backgroundImage: {
-      default: 'white'
-    }
-  }
+      default: "white",
+    },
+  },
 });
 
 function App() {
   return (
     <ThemeProvider theme={themeColor}>
-       <CssBaseline />
-       
-       <Dashboard />       
+      <CssBaseline />
+
+      <Dashboard />
     </ThemeProvider>
->>>>>>> ebf5df3 (trying to solve)
   );
 }
 
