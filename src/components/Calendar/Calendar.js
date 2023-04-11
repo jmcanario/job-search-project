b458621ca8db552cbadfb1c011d57b3d5dfb02d0
Juan Manuel Armas <36412631+jmcanario@users.noreply.github.com>
2023-04-12 00:15:48 +0100

    Add events in different file

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -5,39 +5,14 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import DateModal from './DateModal';
 import { v4 as uuidv4 } from 'uuid';
-import { useState } from "react";
+
+import event from '../../db/Dummy_events';
 
 
 export default function BasicDateTimePicker() {
  
 
-const [event, setEvent] = useState([
-  
-  {
-        id: 'a',
-        title: 'A date',
-        start: '2023-04-01T17:30:00',
-        end: '2023-04-01T19:30',
-      },
-  {
-        id: 'b',
-        title: 'B date',
-        start: '2023-04-07T17:30:00',
-        end: '2023-04-07T19:30',
-      },
-  {
-        id: 'c',
-        title: 'C date',
-        start: '2023-04-03T17:30:00',
-        end: '2023-04-03T19:30',
-      },
-  {
-        id: 'd',
-        title: 'D date',
-        start: '2023-04-21T17:30:00',
-        end: '18:00',
-      },
-])
+
 
   uuidv4();
  
@@ -55,7 +30,7 @@ const [event, setEvent] = useState([
       ...event, newEvent
     ];
     // console.log (newEvents)
-    
+
     setEvent(newEvents);
     };
 
